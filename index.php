44eb45bfc21585a4a72c0796ff468c9e1220d31f
freirediego <diegofg23@outlook.es>
2023-10-13 21:34:32 -0500

    Third functional version

--- index.php
+++ index.php
@@ -3,23 +3,7 @@
 if(isset($_GET['warning']) && $_GET['warning'] == 1) {
   echo '<div class="warning">Warning: This email is already being used!</div>';
 }
-// if($_SERVER["REQUEST_METHOD"]=== "POST"){
-//   extract($_POST);
-//   var_dump($_POST);
 
-//  $user = $_POST['usuario'];
-//  $password = $_POST['password'];
-
-// if($user  && $password){
-//   $_SESSION['usuario'] = $user;
-
-//   header("Location: home.php");
-//   exit();
-// } else {
-//   $error = "Usuario o password incorrecto!";
-// }
-
-// }
 
 // THIS PAGE IS TO CREATE NEW USERS
 ?>
@@ -29,37 +13,52 @@ if(isset($_GET['warning']) && $_GET['warning'] == 1) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/styles/style.css">
   <title>Document</title>
 </head>
 
-<body>
+<body class="test">
+  <div>
+    <main class="login-main">
+      <div class="login-form-cont">
+        <img src="/public/devchallenges.svg" alt="logo" class="dev-logo">
+        <h4>Join thousands of learners from around the world
+        </h4>
 
-  <main>
-    <div>
-      <h4>Join thousands of learners from around the world
-      </h4>
+        <p class="form-info">Master web development by making real-life projects. There are multiple paths
+          for you to choose
+        </p>
 
-      <p>Master web development by making real-life projects. There are multiple paths
-        for you to choose.
-      </p>
+        <form action="/services/create.php" method="post">
+          <div class="input-cont">
+            <img src="/public/letter.svg" alt="icon">
+            <input type="text" placeholder="Email" name="usuario">
+            <br>
+          </div>
 
-      <form action="/services/create.php" method="post">
-
-        <input type="text" placeholder="Email" name="usuario">
-        <br>
-
-        <input type="text" placeholder="Password" name="password">
-        <br>
-        <button type="submit">Start coding now</button>
-        <div>
-          <p>or continue with these social profile</p>
-        </div>
-        <span>Already a member? <a href="/views/loginsite.php">Login</a></span>
+          <div class="input-cont">
+            <img src="/public/lock.svg" alt="icon">
+            <input type="text" placeholder="Password" name="password">
+          </div>
 
 
-      </form>
-    </div>
-  </main>
+          <button class="index-button" type="submit">Start coding now</button>
+          <div class="social-cont">
+            <p>or continue with these social profile</p>
+            <div class="social-prof">
+              <img src="/public/Google.svg" alt="social-profile-img"><img src="/public/Facebook.svg"
+                alt="social-profile-img"><img src="/public/Twitter.svg" alt="social-profile-img"><img
+                src="/public/Gihub.svg" alt="social-profile-img">
+            </div>
+          </div>
+          <div class="member">
+            <span>Already a member? <a href="/views/loginsite.php">Login</a></span>
+          </div>
+
+        </form>
+      </div>
+    </main>
+  </div>
   <script src="/scripts/index.js"></script>
 </body>
 
